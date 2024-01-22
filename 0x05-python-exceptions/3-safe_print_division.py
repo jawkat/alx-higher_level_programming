64ad9c300bc5196820fa704b37f06654a6dbe809
jawkat <kad.joliano@gmail.com>
2024-01-22 15:45:48 +0100

    task 3 2

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         res = None
     finally:
-        print("{res}".format(res))
+        print("Inside result: {}".format(res))
         return res
