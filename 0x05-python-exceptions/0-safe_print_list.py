71905603a7adc4115c70020d085d913850c450b4
jawkat <kad.joliano@gmail.com>
2024-01-22 10:11:38 +0100

    task 0 version 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cout = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            cout+= 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return cout
+    
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+readme relative to python excpetion
