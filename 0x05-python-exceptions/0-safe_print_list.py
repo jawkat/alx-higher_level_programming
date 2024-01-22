118b7e35ef857c45d41b4d95398ac8a962ddd9c9
jawkat <kad.joliano@gmail.com>
2024-01-22 14:07:55 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    cout = 0
-    try:
-        while cout is not x:
-            print(my_list[cout], end='')
-            cout+= 1
-    except IndexError:
-        None
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
     print()
-    return cout
-    
+    return printed
