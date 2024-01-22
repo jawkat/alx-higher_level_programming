b2f443e6439493082f0f8f4f9b89c42bc9cfcb36
jawkat <kad.joliano@gmail.com>
2024-01-22 12:57:48 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     cout = 0
     try:
-        while cont is not x:
+        while cout is not x:
             print(my_list[cout], end='')
             cout+= 1
     except IndexError:
