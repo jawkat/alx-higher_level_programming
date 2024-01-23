acc30083b3d9d5d27002f2d3d2c96abae6164a6a
jawkat <kad.joliano@gmail.com>
2024-01-23 18:29:57 +0100

    task 2 att 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,7 +18,7 @@ class Square:
 
         """
 
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
