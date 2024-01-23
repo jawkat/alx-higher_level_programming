0cb3af2e24ca8e8612d883832a99a8bac15f0347
jawkat <kad.joliano@gmail.com>
2024-01-23 18:38:24 +0100

    task 3 att 1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+
+"""Square and set size as private instance variable"""
+
+
+class Square:
+    """The class to create square."""
+
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size (int): the size of the square
+
+        Raises:
+            TypeError : if size is an integer
+            ValueError: if size < 0
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = area(size)
+        
+        def area(self):
+            """ Area of this square.
+            Returns :
+                the size squared.
+            """
+            return self.__size ** 2
+
+        
