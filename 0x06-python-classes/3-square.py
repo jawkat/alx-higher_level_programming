2056bb7fdb183888cb85e765179ccb42e11e33cf
jawkat <kad.joliano@gmail.com>
2024-01-23 18:39:54 +0100

    task 3 att 2 : correction style

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -22,14 +22,12 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = area(size)
-        
+
         def area(self):
             """ Area of this square.
             Returns :
                 the size squared.
             """
             return self.__size ** 2
-
-        
