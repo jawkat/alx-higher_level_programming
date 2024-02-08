ae6df95b3c19dd5fd603b324a326f9d917b39b25
jawkat <kad.joliano@gmail.com>
2024-02-08 15:44:44 +0100

    pcode style task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,18 +3,18 @@
     verifie is the object is an instance of a class
 """
 
+
 def is_same_class(obj, a_class):
     """function returns True if the object is the same class
 
     Args:
         obj (object): obejct
         a_class (class): class to check
-        
+
     Returns: true if the object is the same class
     """
-    
-    if type(obj) == a_class:
+
+    if isinstance(obj, a_class):
         return True
     else:
         return False
-    
