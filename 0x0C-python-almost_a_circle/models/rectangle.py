32eb6ed9673ce83820a6a562d5d1ee0e2551f236
jawkat <kad.joliano@gmail.com>
2024-02-16 16:27:31 +0100

    jj

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -50,13 +50,3 @@ class Rectangle(Base):
             value (_type_): _description_
         """
         self.__height = value
-
-
-r1 = Rectangle(10, 2)
-print(r1.id)
-
-r2 = Rectangle(2, 10)
-print(r2.id)
-
-r3 = Rectangle(10, 2, 0, 0, 12)
-print(r3.id)
