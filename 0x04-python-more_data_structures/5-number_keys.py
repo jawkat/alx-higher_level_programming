c13805a6bd84ebd17375db9daad9e95a92338253
jawkat <kad.joliano@gmail.com>
2024-02-20 16:15:07 +0100

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+     _summary_
+    """
+
+
+def number_keys(a_dictionary):
+    """
+    number_keys _summary_
+
+    Args:
+        a_dictionary (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return len(a_dictionary)
