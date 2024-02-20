1d7bbe5f372f0f0b20a95acf9b0d4790965b5a1f
jawkat <kad.joliano@gmail.com>
2024-02-20 12:24:04 +0100

    first task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""_summary_"""
+def square_matrix_simple(matrix=[]):
+    """_summary_
+
+    Args:
+        matrix (list, optional): _description_. Defaults to [].
+    """
+    return [[num**2 for num in row] for row in matrix]
