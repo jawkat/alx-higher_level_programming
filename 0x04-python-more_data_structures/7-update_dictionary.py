2e168a6944016987b15bc3f471529fcff5adfb59
jawkat <kad.joliano@gmail.com>
2024-02-20 16:29:12 +0100

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    update_dictionary _summary_
+
+    Args:
+        a_dictionary (_type_): _description_
+        key (_type_): _description_
+        value (_type_): _description_
+    """
+
+    a_dictionary[key] = value
+    return(a_dictionary)
