139a73bb5ff7c750587649393fdd89cb12e06ad0
jawkat <kad.joliano@gmail.com>
2024-02-20 16:09:25 +0100

    task four$

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+     _summary_
+"""
+
+
+def only_diff_elements(set_1, set_2):
+    """
+    only_diff_elements _summary_
+
+    Args:
+        set_1 (_type_): _description_
+        set_2 (_type_): _description_
+    """
+
+    return set_1 ^ set_2
