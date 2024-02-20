767fca20535dc27218794051aff431f0e2bcf7cf
jawkat <kad.joliano@gmail.com>
2024-02-20 16:03:09 +0100

    task common element in twoset

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+     common elemnt in two sets
+"""
+def common_elements(set_1, set_2):
+    """
+    common_elements _summary_
+
+    Args:
+        set_1 (_type_): _description_
+        set_2 (_type_): _description_
+    """
+
+    return set_1 & set_2
