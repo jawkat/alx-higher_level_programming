5ca6ac1b620f8c94259fbadf79c1606f9bf2336c
jawkat <kad.joliano@gmail.com>
2024-04-05 11:28:10 +0000

    task 7 styling

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,8 +5,7 @@
 class Rectangle:
     """Represents a rectangle. No body."""
     number_of_instances = 0
-    print_symbol ="#"
-
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
