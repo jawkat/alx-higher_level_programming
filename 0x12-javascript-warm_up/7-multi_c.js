17532cbbdab1581fb0c4a54466ccc7c710844924
jawkat <kad.joliano@gmail.com>
2024-04-08 13:44:52 +0000

    task [H

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (parseInt(process.argv[2]) === NaN) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
