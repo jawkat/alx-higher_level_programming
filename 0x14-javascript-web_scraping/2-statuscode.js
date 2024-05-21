31d8cc53c7ee31ca64a2324ad380328d6e41c56c
jawkat <kad.joliano@gmail.com>
2024-05-21 15:17:47 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 
 const url = process.argv[2];
-request(url, function (error, response, body) {
-  console.log('code: ', response.statusCode || error);
+request(url, (req, res) => {
+  console.log('code: ', res.statusCode);
 });
